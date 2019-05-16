7b9a3b2bf4bb688b294cde4fc097c7df7f3acf71
tao <a751029@hotmail.com>
2019-05-16 17:12:51 +0800

    U114

--- AlphaPos/app.js
+++ AlphaPos/app.js
@@ -3,3 +3,12 @@
 // 列出調整甜度的選項，一次只能選擇一個選項，預設是正常甜(Regular)
 // 點擊新增(Add) 按鈕後，會得到店員所點選的項目，並且顯示於左側飲料訂單區
 // 若沒有選擇飲料品項就直接點選新增按鈕，會出現錯誤提示：「請至少選擇一種飲料」
+
+// constructor function for drink
+function Drink(name, sugar, ice) {
+  this.name = name
+  this.sugar = sugar
+  this.ice = ice
+}
+
+let bubbleMilkTea = new Drink('Bubble Milk Tea', 'No sugar', 'Less Ice')
